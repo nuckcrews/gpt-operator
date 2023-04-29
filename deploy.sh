b3f9765b5febcb2dddb502b1b528a21be8301cdc
Nick Crews <nickcrews35@gmail.com>
2023-04-29 02:09:22 -0400

    Upgrade to 0.0.7

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ rm -r dist
 rm -r build
 rm -r gptop.egg-info
 python setup.py bdist_wheel
+python -m twine upload dist/*
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gptop",
-    version="0.0.6",
+    version="0.0.7",
     author="Nick Crews",
     author_email="nickcrews35@gmail.com",
     url="https://github.com/ncrews35/gpt-operator",
