0296c7637c79a3ba62c834931aa0404fa6a96d08
Nick Crews <nickcrews35@gmail.com>
2023-04-30 23:32:02 -0400

    Refactor GPT edits

--- cli/__init__.py
+++ cli/__init__.py
@@ -8,12 +8,9 @@ load_dotenv()
 
 # Initialize GPTOP with API keys and other configurations
 gptop.init(
-    openai_key=os.environ.get("OPENAI_API_KEY"),  # Changed from os.getenv to os.environ.get for better security
-    pinecone_key=os.environ.get("PINECONE_API_KEY"),  # Changed from os.getenv to os.environ.get for better security
-    pinecone_region=os.environ.get("PINECONE_REGION"),  # Changed from os.getenv to os.environ.get for better security
-    pinecone_index=os.environ.get("PINECONE_INDEX"),  # Changed from os.getenv to os.environ.get for better security
+    openai_key=os.environ.get("OPENAI_API_KEY"),
+    pinecone_key=os.environ.get("PINECONE_API_KEY"),
+    pinecone_region=os.environ.get("PINECONE_REGION"),
+    pinecone_index=os.environ.get("PINECONE_INDEX"),
     operations_types=[HTTP]
 )
-
-# Change: Replaced os.getenv with os.environ.get to avoid potential security vulnerabilities
-# Reason: os.environ.get is safer as it does not fallback to os.environ, which could lead to unintended behavior.
--- cli/__main__.py
+++ cli/__main__.py
@@ -2,8 +2,4 @@
 import cli.app as app
 
 if __name__ == "__main__":
-    # Added try-except block to handle exceptions and prevent potential security vulnerabilities
-    try:
-        app.main()
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    app.main()
