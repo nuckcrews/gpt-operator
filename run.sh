cb8e2b2b04b9a0252e19ac728ed1a0c24e458262
Nick Crews <nickcrews35@gmail.com>
2023-04-30 20:02:34 -0400

    Revert some GPT changes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 python scripts/check_requirements.py requirements.txt
 if [ $? -eq 1 ]
 then
-    echo "Installing missing packages..."
+    echo Installing missing packages...
     pip install -r requirements.txt
 fi
 python -m cli $@
